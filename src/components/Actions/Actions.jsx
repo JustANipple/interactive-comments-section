f96308d40ea9bfff1bb5f47ac1dc844d686b8ebd
JustANipple <prandisamuel99@gmail.com>
2023-08-03 16:29:38 +0200

    modified:   src/components/Actions/Actions.jsx 	modified:   src/components/Post/Post.jsx

--- src/components/Actions/Actions.jsx
+++ src/components/Actions/Actions.jsx
@@ -1,50 +1,92 @@
 import styles from "./Actions.module.css"
+import postStyles from "../Post/Post.module.css"
 
 /*
     TODO:
     2 types of buttons (post, interact)
     post btn: reply, update, send
     interact btn: reply, edit, delete
+    delete section btn: cancel, delete
 */
 
 // eslint-disable-next-line react/prop-types
-const Actions = ({ category, type }) => {
-
-    function handleSend() {}
-    function handleUpdate() {}
-    function handleReply() {}
-    function handleDelete() {}
-    function handleEdit() {}
+const Actions = ({ category, type, data, setData, currentUser }) => {
 
     function buildUrl(type) {
         return `images/icon-${type}.svg`;
     }
-
-    function postBtn() {
+    
+    function handleEvents(category, type, e) {
+        //post events: postReply, update, send
+        //act events: actReply, edit, delete
+        if(type === "send") {
+            handleSend(e);
+        } else if (type === "delete") {
+            handleDelete(e);
+        }
     }
 
-    function actBtn() {
+    function handleSend(e) {
+        e.preventDefault();
+        const textContent = e.target.parentElement.parentElement.querySelector("textarea").value;
+        if(textContent !== "") {
+            //Clone comments array
+            // eslint-disable-next-line react/prop-types
+            const updatedComments = [...data.comments];
+            //Push new comment into the cloned array
+            updatedComments.push(
+                {
+                    // eslint-disable-next-line react/prop-types
+                    "id": data.comments.length + 1,
+                    "content": textContent,
+                    "createdAt": "now",
+                    "score": 0,
+                    "user": {
+                    "image": { 
+                        // eslint-disable-next-line react/prop-types
+                        "png": currentUser.image.png,
+                        // eslint-disable-next-line react/prop-types
+                        "webp": currentUser.image.webp
+                    },
+                    // eslint-disable-next-line react/prop-types
+                    "username": currentUser.username
+                    },
+                    "replies": []
+                }
+            )
+            const newData = {...data, comments: updatedComments};
+            //Set the state with updated array
+            setData(newData);
+            //Save new state in localStorage
+            localStorage.setItem("data", JSON.stringify(newData));
+        }
     }
+
+    function handleDelete(e) {
+    }
+
+
+
+    function handleUpdate() {}
+    function handleReply() {}
+
+    function handleEdit() {}
 
     return (
         <button 
-            className={category === "post" ? 
-                        styles.post_btn : 
-                        styles.action_button}
-        >
+            className={`${category === "post" ? postStyles.post_btn : styles.action_button}`}
+            onClick={(e) => handleEvents(category, type, e)}
+            >
             {category === "act" ? 
-                <>
+            <>
                 <img 
                     src={buildUrl(type)}
-                    alt={type + "icon"}
+                    alt={type + " icon"}
                 />
-                <p 
-                    className = {`${styles.button_type} ${type === "delete" ? 
-                                    styles.delete :
-                                    ""}`}
-                >{type}</p>
-                </>
-            : {type} }
+                {/* eslint-disable-next-line react/prop-types */}
+                <p className = {`${styles.button_type} ${type.includes("delete") ? styles.delete : "" } ${type.includes("cancel") ? styles.cancel : ""}`}>{type}</p>
+            </>
+            : type }
         </button>
     )
 }
--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -4,42 +4,6 @@ import styles from "./Post.module.css"
 // eslint-disable-next-line react/prop-types
 const Post = ({ currentUser, type, data, setData }) => {
 
-    function handleClick(e) {
-        e.preventDefault();
-        const textContent = e.target.parentElement.parentElement.querySelector("textarea").value;
-        if(textContent !== "") {
-            //Clone comments array
-            // eslint-disable-next-line react/prop-types
-            const updatedComments = [...data.comments];
-            //Push new comment into the cloned array
-            updatedComments.push(
-                {
-                    // eslint-disable-next-line react/prop-types
-                    "id": data.comments.length + 1,
-                    "content": textContent,
-                    "createdAt": "now",
-                    "score": 0,
-                    "user": {
-                    "image": { 
-                        // eslint-disable-next-line react/prop-types
-                        "png": currentUser.image.png,
-                        // eslint-disable-next-line react/prop-types
-                        "webp": currentUser.image.webp
-                    },
-                    // eslint-disable-next-line react/prop-types
-                    "username": currentUser.username
-                    },
-                    "replies": []
-                }
-            )
-            const newData = {...data, comments: updatedComments};
-            //Set the state with updated array
-            setData(newData);
-            //Save new state in localStorage
-            localStorage.setItem("data", JSON.stringify(newData));
-            }
-    }
-
     return (
         <form className={styles.post}>
             <label htmlFor="comment">
@@ -62,10 +26,13 @@ const Post = ({ currentUser, type, data, setData }) => {
                 id="comment"
                 placeholder="Add a comment..."
             ></textarea>
-            <button 
-                className={styles.post_btn}
-                onClick={(e) => handleClick(e)}
-            >{type}</button>
+            <Actions 
+                category={"post"}
+                type={type}
+                data={data}
+                setData={setData}
+                currentUser={currentUser}
+            />
         </form>
     )
 }
